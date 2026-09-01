e57445290a68f927c090e0067311f65a95c6dc18
alexnastgr <alexnastgr@gmail.com>
2026-09-01 20:58:53 +0300

    refactor: reorganize Booting component structure and improve loading logic

--- src/screens/Booting.tsx
+++ src/screens/Booting.tsx
@@ -1,49 +1,42 @@
 import "@/styles/booting.css";
-
-
 import { Icon } from "@iconify/react";
-import { useBootSequence } from "@/hooks/useBootSequence";
 
+import { useEffect } from "react";
 import { useScreen } from "@/hooks/useScreen";
+import { useBootSequence } from "@/hooks/useBootSequence";
 
 export default function Booting() {
   const { step, isReady } = useBootSequence();
+  const { gotoScreen } = useScreen();
 
+  useEffect(() => {
+    if (isReady) {
+      gotoScreen("auth");
+    }
+  }, [isReady, gotoScreen]);
 
-  const {gotoScreen} = useScreen();
-
-  if (!isReady) {
-    // first time system boots
-    return (
-      <div className="bootScreen font-default">
-        {/* boot animation */}
-        <div className="content text-white">
-          <div className="logo-boot">
-            <Icon icon="mdi:apple" width={60} />
-          </div>
-          {/* current step string */}
-          <p className="text-sm">{step}...</p>
-        </div>
-      </div>
-    );
-  }
-
-  //  first time ready screen
   return (
-    <div className="readyScreen bg-url[(images/ready.jpg)]">
+    <div className="bootScreen">
       <div className="content text-white">
         <div>
-          <Icon icon="mdi:apple" width={80} className="spin" />
+          <Icon
+            icon="mdi:apple"
+            width={isReady ? 80 : 50}
+            className={isReady ? "spin" : "pulse"}
+          />
         </div>
 
-        {/* welcome message */}
-        <div className="flex flex-row gap-2 text-3xl font-norican">
-          <div className="font-notmal">Welcome to</div>
-          <div className="font-semibold">SlimOS</div>
+        <div
+          className={`${isReady ? "flex" : "hidden"} flex-col gap-2 text-center`}
+        >
+          <div className="font-light text-3xl">Welcome to SlimOS</div>
+
+          <div className="font-semibold text-sm">
+            Modern OS experience inspired by macOS
+          </div>
         </div>
 
-        {/* navigate to main desktop area */}
-        <div className="btnDesktop" onClick={()=>{gotoScreen("desktop")}}>Load My Desktop</div>
+        <div>{step}</div>
       </div>
     </div>
   );
