eb599c5cd257e7ed5d5e14d3e72e9c808724cff7
alexnastgr <alexnastgr@gmail.com>
2026-09-02 04:05:09 +0300

    fix: correct import path for IUser type and format usersList entries

--- src/data/users.ts
+++ src/data/users.ts
@@ -1,12 +1,20 @@
-import { type IUser } from "@/types/users";
+import type { IUser } from "@/types/user";
 
 export const usersList: IUser[] = [
   {
-    uid:1034,
+    uid: 1034,
     name: "John Doe",
     username: "johndoe",
     password: "12345",
     avatar: "1.png",
     userType: "admin",
-  }
+  },
+  {
+    uid: 1035,
+    name: "Jane Doe",
+    username: "janedoe",
+    password: "12345",
+    avatar: "2.png",
+    userType: "user",
+  },
 ];
