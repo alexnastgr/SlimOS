e40d61b6b1c390182181c421f47bb668fcbe68ca
alexnastgr <alexnastgr@gmail.com>
2026-09-03 21:38:09 +0300

    feat: update LeftPanel to display current user name or "Guest"

--- src/components/_taskbar/Panels/LeftPanel.tsx
+++ src/components/_taskbar/Panels/LeftPanel.tsx
@@ -1,14 +1,19 @@
 import { Icon } from "@iconify/react";
 
+import { useSession } from "@/hooks/useSession";
 function LeftPanel() {
+  const { currentUser } = useSession();
+
   return (
-    <div className="leftPanel text-black">
+    <div className="leftPanel">
       <div className="text-primary">
         <Icon icon="solar:box-minimalistic-bold-duotone" width={23} />
       </div>
 
-      {/* show focused app name */}
-      <div className="font-semibold">Finder</div>
+      {/* user name */}
+      <div className="font-semibold text-sm">
+        {currentUser?.name || "Guest"}
+      </div>
     </div>
   );
 }
