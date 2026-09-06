af83b0dae8d6c64fee719986c21de5c585629cae
alexnastgr <alexnastgr@gmail.com>
2026-09-06 12:58:15 +0300

    fix: display dynamic error icon by error type

--- src/components/ErrorMessage.tsx
+++ src/components/ErrorMessage.tsx
@@ -1,4 +1,6 @@
 import { motion } from "framer-motion";
+import { Icon } from "@iconify/react";
+
 import type { MessageType } from "@/types/messages";
 
 interface Props {
@@ -6,23 +8,29 @@ interface Props {
   type: MessageType;
 }
 
-export default function ErrorMessage({ message, type }: Props) {
-  const styles: Record<MessageType, string> = {
-    error: "bg-red-500",
-    warning: "bg-yellow-500",
-    success: "bg-green-500",
-  };
+const styles: Record<MessageType, string> = {
+  error: "bg-red-500",
+  warning: "bg-yellow-500",
+  success: "bg-green-500",
+};
 
+const icons: Record<MessageType, string> = {
+  error: "mdi:alert-circle",
+  warning: "mdi:alert",
+  success: "mdi:check-circle",
+};
+
+export default function ErrorMessage({ message, type }: Props) {
   return (
     <motion.div
       initial={{ opacity: 0, y: -10 }}
       animate={{ opacity: 1, y: 0 }}
       exit={{ opacity: 0, y: -10 }}
       transition={{ duration: 0.3 }}
-      className={`errorMsg
-        ${styles[type]}
-      `}
+      className={`errorMsg ${styles[type]}`}
     >
+      <Icon icon={icons[type]} className="mr-2" />
+
       {message}
     </motion.div>
   );
