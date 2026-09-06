4e611bc74d26dcf13037d60949b299efdd11bc01
alexnastgr <alexnastgr@gmail.com>
2026-09-06 17:32:41 +0300

    feat: support last user session , lock & unlock user functionality

--- src/store/slices/sessionSlice.ts
+++ src/store/slices/sessionSlice.ts
@@ -1,14 +1,22 @@
 import { createSlice, type PayloadAction } from "@reduxjs/toolkit";
 import type { IUser } from "@/types/user";
 
+type SessionAction = "login" | "logout" | null;
+
 interface SessionState {
   currentUser: IUser | null;
+  lastUser: IUser | null;
   isAuthenticated: boolean;
+  isLocked: boolean;
+  action: SessionAction;
 }
 
 const initialState: SessionState = {
   currentUser: null,
+  lastUser: null,
   isAuthenticated: false,
+  isLocked: false,
+  action: null,
 };
 
 const sessionSlice = createSlice({
@@ -18,16 +26,31 @@ const sessionSlice = createSlice({
   reducers: {
     login(state, action: PayloadAction<IUser>) {
       state.currentUser = action.payload;
+      state.lastUser = action.payload;
       state.isAuthenticated = true;
+      state.action = "login";
     },
 
     logout(state) {
+      state.lastUser = state.currentUser;
       state.currentUser = null;
       state.isAuthenticated = false;
+      state.isLocked = false;
+      state.action = "logout";
+    },
+
+    // just lock the user
+    lock(state) {
+      if (state.isAuthenticated) {
+        state.isLocked = true;
+      }
+    },
+    // unvlock the user
+    unlock(state) {
+      state.isLocked = false;
     },
   },
 });
 
-export const { login, logout } = sessionSlice.actions;
-
+export const { login, logout, lock, unlock } = sessionSlice.actions;
 export default sessionSlice.reducer;
