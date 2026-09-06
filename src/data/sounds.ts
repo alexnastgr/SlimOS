f3259746c69e7e87b03f5eafc0b74279271bf12b
alexnastgr <alexnastgr@gmail.com>
2026-09-06 06:19:28 +0300

    Add logout sound to the sounds collection

--- src/data/sounds.ts
+++ src/data/sounds.ts
@@ -1,12 +1,18 @@
 import { type ISound } from "@/types/sound";
 
 import welcome from "@/assets/sounds/welcome.mp3";
+import logout from "@/assets/sounds/logout.mp3";
 import error from "@/assets/sounds/error.mp3";
 
 export const Sounds: ISound[] = [
   {
     name: "welcome",
     source: welcome,
+  },
+
+  {
+    name: "logout",
+    source: logout,
   },
   {
     name: "error",
